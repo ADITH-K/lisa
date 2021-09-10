79203fa0e4d9c65c50eb6777d82118a40117a018
Salman dx <88703871+salman-dx@users.noreply.github.com>
2021-09-10 21:16:07 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -21,9 +21,9 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ameer Suhail*
 *owner number wa.me/918129727300*
 *bot setting video : https://youtu.be/_D4ZYuUSXjs
-*githublink       https://github.com/ameer-kallumthodi/pikachu*
-*audio commads    https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*sticker commads  https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*githublink       https://github.com/salman-dx/lisa*
+*audio commads    https://github.com/salman-dx/lisa/tree/master/uploads*
+*sticker commads  https://github.com/salman-dx/lisa/tree/master/uploads*
 `}) 
 
 }));
