15d0ea68f08e3fb61d7e9af464fbded995ac47e5
Salman dx <88703871+salman-dx@users.noreply.github.com>
2021-09-10 18:25:38 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -818,7 +818,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                                     '\n*lisa an error has occurred!*'+
                                     '\n*This error log may include your number or the number of an opponent. Please be careful with it!*' +
                                     '\n*You can write to our Telegram group for help.*' +
-                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/FsDjV2uRKce4wgMpAtYwyf' +
+                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/If8MlMpo2uFI4KFlYqSF19' +
                                     '\n*This message should have gone to your number (saved messages).*\n\n' +
                                     '*Error:* ```' + error + '```\n\n'
                                     , MessageType.text, {detectLinks: false}
