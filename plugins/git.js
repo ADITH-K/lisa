2951bbc2d8c07e1a4c0aa697f97448dd37090718
Salman dx <88703871+salman-dx@users.noreply.github.com>
2021-09-11 14:31:58 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,12 +18,12 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ameer Suhail*
-*owner number wa.me/918129727300*
-*bot setting video : https://youtu.be/_D4ZYuUSXjs
-*githublink       https://github.com/salman-dx/lisa*
-*audio commads    https://github.com/salman-dx/lisa/tree/master/uploads*
-*sticker commads  https://github.com/salman-dx/lisa/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is salman dx*
+*Creator number : wa.me/918129727300?text=Hi%20salman%20dx.%20*
+*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*Githublink (Setup)  :    https://github.com/salman-dx/lisa*
+*Audio commads :   https://github.com/salman-dx/lisa/tree/master/uploads*
+*Sticker commads : https://github.com/salman-dx/lisa/tree/master/uploads*
 `}) 
 
 }));
